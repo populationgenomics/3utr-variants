f63468fcf4e2730df4479895169e609660b267c6
Michaela Mueller <michaela.mueller@populationgenomics.org.au>
2021-02-25 19:48:22 +1100

    Setup basic package

---
+++ setup.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-module-docstring
+from setuptools import setup
+
+setup(
+    name='utr3variants',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='mumichae',
+    author_email='michaela.mueller@populationgeomics.org.au',
+    description="Functions for evaluating effect of 3'UTR variants on genome",
+)
