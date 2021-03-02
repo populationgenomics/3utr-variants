5d3801b03f8fb9ac25436e09c754fd0fcf35906b
Michaela Mueller <michaela.mueller@populationgenomics.org.au>
2021-03-02 16:42:42 +1100

    Update plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -10,18 +10,30 @@ from plotnine import *  # pylint: disable=W0614,W0401
 
 df = pd.read_csv(snakemake.input[0], sep='\t')
 
+df = df[df.worst_csq == '3_prime_UTR_variant']
+df[['interval_set', 'annotation']] = df['UTR_group'].str.split('|', expand=True)
+
+title = snakemake.config['gnomAD']['subset']
+point_position = position_dodge(1)
+
 maps_plot = (
     ggplot(df)
-    + aes('worst_csq', 'maps', color='protein_coding')
-    + geom_point()
+    + aes('interval_set', 'maps', color='annotation')
+    + geom_point(position=point_position)
+    + geom_errorbar(
+        aes(ymin='maps-maps_sem', ymax='maps+maps_sem'),
+        width=0.1,
+        position=point_position,
+    )
+    + geom_text(aes(label='variant_count'), position=point_position)
     + geom_hline(yintercept=0, color='grey')
     + facet_grid('variant_subset~.')
-    + ggtitle(title=snakemake.config['gnomAD']['subset'])
-    + theme_bw()
-    + scale_color_brewer(type="qual", palette='Set1')
+    + ggtitle(title=title)
+    + scale_color_brewer(type="qual", palette='Dark2')
+    + theme_classic()
     + theme(
-        legend_position='top',
-        axis_text_x=element_text(angle=90, hjust=1),
+        legend_position='right',
+        # axis_text_x=element_text(angle=90, hjust=1),
         axis_ticks=element_blank(),
     )
 )
