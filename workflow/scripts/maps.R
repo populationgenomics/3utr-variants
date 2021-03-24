717f300164ac485bb54243a4510c501e5fd9e581
Michaela Mueller <michaela.mueller@populationgenomics.org.au>
2021-03-24 16:54:13 +1100

    Adapted plotting

--- workflow/scripts/maps.R
+++ workflow/scripts/maps.R
@@ -9,34 +9,37 @@ source(snakemake@input$utils)
 
 count_dt <- fread(snakemake@input$counts)
 
+# get reference MAPS
+dt_csq <- maps_reference(count_dt)
+
 # get aggregation MAPS
 aggregations <- strsplit(snakemake@wildcards$aggregation, '-')[[1]]
 dt <- maps(count_dt, grouping = aggregations)
 fwrite(dt, snakemake@output$tsv, sep = '\t')
 
-# get reference MAPS
-dt_csq <- maps_reference(count_dt)
-
-# Plot only top 100 variants
-dt <- dt[variant_count > 100]
+# Plot only top n variants
+dt <- dt[variant_count > snakemake@params$variant_count_min]
 setorderv(dt, cols = aggregations)
 
 new_columns <- c('anno_x', 'shape', 'facet')[seq_along(aggregations)]
+
+# Flatten for 3'UTR and other variants
+if ('feature' %in% names(dt) & length(aggregations) == 3) {
+  flatten_values <- c('3UTR', '')
+  dt_lines <- dt[feature %in% flatten_values]
+  dt <- dt[!feature %in% flatten_values]
+  setnames(dt_lines, old = aggregations, new = new_columns)
+}
+
 setnames(dt, old = aggregations, new = new_columns)
 # dt[, anno_x := paste0(anno_x, '\n(', variant_count, ')')]
-
-# Flatten for certain x-values
-if ('shape' %in% names(dt) & aggregations[1] %in% c('hexamer', 'conserved')) {
-  dt_lines <- dt[shape %in% anno_x]
-  dt <- dt[!shape %in% anno_x]
-}
 
 title <- paste0('MAPS on ', snakemake@wildcards$chr_subset,
                 ' (', snakemake@params$chr_subset, ')')
 dodge_width <- 0.8
 
 if ('shape' %in% names(dt)) {
-  p <- ggplot(dt, aes(reorder(anno_x, variant_count), maps, shape = shape, group = shape))
+  p <- ggplot(dt, aes(reorder(anno_x, variant_count), maps, color = shape, group = shape))
 } else {
   p <- ggplot(dt, aes(reorder(anno_x, variant_count), maps))
 }
@@ -49,17 +52,22 @@ if ('facet' %in% names(dt)) {
 
 if (exists('dt_lines')) {
   p <- p + geom_hline(
-      aes(yintercept = maps, color = shape),
-      data = dt_lines[, .(maps, shape)]
-    )
+    aes(yintercept = maps, color = shape),
+    data = dt_lines[, .(maps, shape)]
+  )
 }
 
 p <- p +
   geom_hline(yintercept = 0, colour = 'grey50', linetype = 'dotted') +
   geom_hline(  # add lines for reference
-    aes(yintercept = maps, color = consequence),
+    aes(yintercept = maps, group = consequence),
     data = dt_csq[, .(maps, consequence)],
-    linetype = 'dashed'
+    linetype = 'dashed',
+    na.rm = TRUE
+  ) +
+  geom_text(
+    aes(x = '', y = maps, label = consequence, vjust = -0.3),
+    data = dt_csq, inherit.aes = FALSE, nudge_x = -0.3, size = 3
   ) +
   geom_errorbar(
     aes(ymin = maps - maps_sem, ymax = maps + maps_sem),
@@ -71,8 +79,7 @@ p <- p +
     title = title,
     x = toTitleCase(aggregations[1]),
     y = 'MAPS',
-    shape = toTitleCase(aggregations[2]),
-    color = 'Consequence'
+    color = toTitleCase(aggregations[2])
   ) +
   scale_color_brewer(palette = 'Set1') +
   theme_classic() +
@@ -81,5 +88,5 @@ p <- p +
     axis.text.x = element_text(angle = 90, hjust = 1),
     axis.ticks = element_blank(),
   )
-
+p
 ggsave(snakemake@output$png, width = 8, height = 6)
